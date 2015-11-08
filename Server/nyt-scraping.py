619c0722f1f56e31f94e66a480541db37a8d7727
Hideaki Tamori <hideaki@Hideaki-no-MacBook-Pro.local>
2015-11-07 16:59:57 -0800

    0500

--- Server/nyt-scraping.py
+++ Server/nyt-scraping.py
@@ -6,25 +6,27 @@ import time
 
 api = articleAPI('a3de91f34b805b03d5b06d3478813176:4:72566137')
 
-
-#articles = api.search( fq = {'source':'The New York Times', 'news_desk':'Travel', 'headline':'36 Hours'}, fl = {'web_url', 'headline'}, page = 0)
-#print articles
-	
-#data = urllib.urlencode(values)
-#req = urllib2.Request(url, data)
-#response = urllib2.urlopen(req)
-#the_page = response.read()	
-#print the_page
-
+filename = "url.txt"
+f = open(filename, 'w')
+# To get JSON File written the url of "36 Hours" articles and save to file named [0-70].json
 for i in range(0,71):
 	articles = api.search( fq = {'source':'The New York Times', 'news_desk':'Travel', 'headline':'36 Hours'}, fl = ['web_url', 'headline'], page = i)
 	print articles
-	filename = str(i) + '.json'
-	f = open(filename, 'w')
-	f.write(str(articles))
-	f.close
+	for article in articles['response']['docs']:
+		url = article['web_url']
+		title = article['headline']['main']
+		#print url
+		if url.find('video') == -1 and url.find('interactive') == -1 and url.find('slideshow') == -1 and title.find('36 Hours in') != -1:
+			#print article['headline']['main']
+			#print article['web_url']
+			try:
+				f.write("'" + article['web_url'] + "','" + article['headline']['main'] + "'\n")
+			except:
+				print "ERROR"
+	#filename = str(i) + '.json'
+	#f = open(filename, 'w')
+	#f.write(str(articles))
+	#f.close
 	time.sleep(1)
-#	print url
-#	response = urllib2.urlopen(url)
-#	html = response.read()
-#	print html
+	
+f.close()
