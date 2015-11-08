fd9b0dc76b2b5b74d3c230a9fbab6e9a04580773
Hideaki Tamori <hideaki@Hideaki-no-MacBook-Pro.local>
2015-11-07 19:01:44 -0800

    0700

--- Server/flightaware.py
+++ Server/flightaware.py
@@ -5,6 +5,7 @@ from suds import null, WebFault
 from suds.client import Client
 import logging
 import json
+import urllib
 import urllib2
 import random
 
@@ -41,7 +42,27 @@ def flightaware(IATACode):
 	result = api.service.Scheduled(icao, 20, '', 40)
 	#print result
 	flights = result['scheduled'][random.randint(0,10)]
-	jsontxt = '{ "ident":"%s", "aircrafttype":"%s", "filed_departuretime":%d, "estimatedarrivaltime":%d, "origin":"%s", "destination":"%s", "originName":"%s", "originCity":"%s", "destinationName":"%s", "destinationCity":"%s"}' % (flights['ident'],flights['aircrafttype'],flights['filed_departuretime'],flights['estimatedarrivaltime'],flights['origin'],flights['destination'],flights['originName'],flights['originCity'],flights['destinationName'],flights['destinationCity'])
+	origName = flights['originCity']
+	destName = flights['destinationCity']
+	print origName, destName
+	
+	imgurl_orig = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + urllib.quote(origName, safe='') + "&rsz=8"
+	imgurl_dest = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + urllib.quote(destName, safe='') + "&rsz=8"
+	r_o = urllib2.urlopen(imgurl_orig)
+	root_o = json.loads(r_o.read())
+	r_d = urllib2.urlopen(imgurl_dest)
+	root_d = json.loads(r_d.read())
+	imgurl_o_0 = root_o["responseData"]["results"][0]["url"]
+	imgurl_o_1 = root_o["responseData"]["results"][1]["url"]
+	imgurl_o_2 = root_o["responseData"]["results"][2]["url"]
+	imgurl_d_0 = root_d["responseData"]["results"][0]["url"]
+	imgurl_d_1 = root_d["responseData"]["results"][1]["url"]
+	imgurl_d_2 = root_d["responseData"]["results"][2]["url"]
+	#print imgurl_orig
+	#print imgurl_dest
+	#print imgurl_dest
+	
+	jsontxt = '{ "ident":"%s", "aircrafttype":"%s", "filed_departuretime":%d, "estimatedarrivaltime":%d, "origin":"%s", "destination":"%s", "originName":"%s", "originCity":"%s", "destinationName":"%s", "destinationCity":"%s", "origCityImgUrl":["%s", "%s", "%s"], "destCityImgUrl":["%s","%s","%s"]}' % (flights['ident'],flights['aircrafttype'],flights['filed_departuretime'],flights['estimatedarrivaltime'],flights['origin'],flights['destination'],flights['originName'],flights['originCity'],flights['destinationName'],flights['destinationCity'],imgurl_o_0,imgurl_o_1,imgurl_o_2,imgurl_d_0,imgurl_d_1,imgurl_d_2)
 	print jsontxt
 	return jsontxt
 
